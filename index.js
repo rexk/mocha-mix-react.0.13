7a55d01ee6371fd92eb71945a0e6cf39ce86ae32
rexk <myungsunrex@gmail.com>
2016-01-18 21:41:04 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var React = require('react');
+var createStubReactClass = require('./lib/createStubReactClass');
+
+function ReactClassGenerator(options) {
+  options = options || {};
+  var mockName = options.mockName;
+  var tagName = options.tagName || 'DIV';
+  return createStubReactClass(tagName, mockName);
+}
+
+module.exports = function reactPlugin(mochaMix) {
+  mochaMix.setDefaultMockGenerator(ReactClassGenerator);
+
+  mochaMix.afterEach(function cleanUp() {
+    if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      var testDivs = document.body.childNodes;
+      var result = Array.prototype.slice.call(testDivs).every(function (div) {
+        return React.unmountComponentAtNode(div);
+      });
+      document.body.innerHTML = '';
+    }
+  });
+};
